f6e646a2d21eaf69a843bcb6d29c55d714d85aaf
vwjames <vwjames@sfu.ca>
2017-07-17 13:24:24 -0700

    Add files via upload

---
+++ Invasive_website-master/js/main.js
@@ -0,0 +1,21 @@
+//Manin JavaScript file
+function openCity(evt, cityName) {
+
+    document.getElementById("defaultOpen").click();
+
+    var i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+
+}
