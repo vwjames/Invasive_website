bd30500d2e89d8bb1c42346100741629cd782b9e
cla311 <cla311@sfu.ca>
2017-07-16 18:38:52 -0700

    File Organization

---
+++ js/species.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $(".slideshow").slick({
+    dots: true,
+    autoplay: true,
+    speed: 500,
+    autoplaySpeed: 3000,
+    fade: true,
+    arrows: false,
+    cssEase: 'linear',
+    infinite: true
+  });
+});
