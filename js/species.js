9d80fc4529d1a504e950776c8e77e87216578548
cla311 <cla311@sfu.ca>
2017-07-17 21:27:37 -0700

    Fixed the Slideshow

--- js/species.js
+++ js/species.js
@@ -1,12 +1,14 @@
-$(document).ready(function(){
-  $(".slideshow").slick({
-    dots: true,
-    autoplay: true,
-    speed: 500,
-    autoplaySpeed: 3000,
-    fade: true,
-    arrows: false,
-    cssEase: 'linear',
-    infinite: true
-  });
+'use strict';
+
+$(document).ready(function () {
+    $('.slideshow').slick({
+        dots: true,
+        autoplay: true,
+        speed: 500,
+        autoplaySpeed: 3000,
+        fade: true,
+        arrows: false,
+        cssEase: 'linear',
+        infinite: true
+    });
 });
